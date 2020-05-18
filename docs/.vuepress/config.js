fa088dc9dbb5bbba232ebd3ea460449298adc2b9
go2run <qzr18566783717@foxmail.com>
2020-05-19 05:08:04 +0800

    cahnge back to public as dist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   title: "面朝大海，春暖花开",
   description: 'Enjoy when you can, and endure when you must.',
-  dest: 'docs/.vuepress/dist',  // 设置输出目录
+  dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
